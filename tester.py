f1942f66cee286c2422004b3a6729cceca447f73
kassybas <ubuntu@vag-vm>
2017-08-24 10:35:36 +0000

    More words

---
+++ tester.py
@@ -0,0 +1,33 @@
+import yaml
+import random
+words = {}
+
+class WordPair(object):
+    def __init__(self, tur, eng):
+        self.tur = tur
+        self.eng = eng
+
+def load_words():
+    words = list()
+    with open('words.yaml') as f:
+        d = yaml.load(f)
+        for k, v in d.items():
+            words.append(WordPair(k,v))
+        return words
+
+
+def main():
+    d = load_words()
+    limit = len (d)
+    for i in range(10):
+        print("\n - - - - - - - - - - -")
+        print(i+1)
+        x = random.randrange(0, limit)
+        print("Eng: " + d[x].eng)
+        input("Tip: ")
+        print("Tur: " + d[x].tur)
+        print("\n - - - - - - - - - - -")
+
+if __name__=="__main__":
+    main()
+
